557a470ab4563a09b01df30c2c8828b59fd0f59d
smhrd <smhrd@DESKTOP-JI8Q0GP>
2023-10-18 10:30:56 +0900

    2023.10.18 10:30 프로젝트 첫번째 커밋 (조정흠)

---
+++ ProjectShake/src/main/java/com/smhrd/shake/controller/MainController.java
@@ -0,0 +1,12 @@
+package com.smhrd.shake.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {	
+	@GetMapping("/")
+	public String introPage() {
+		return "intro";
+	}
+}
